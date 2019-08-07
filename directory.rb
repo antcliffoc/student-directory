e04088c16e2de15ed55383b331299ac378e52cf5
Oliver Antcliff <antcliffoc@gmail.com>
2019-08-07 13:17:14 +0100

    clean up unnecessary comments and spaces, increase ligibility

--- directory.rb
+++ directory.rb
@@ -1,13 +1,8 @@
-
-# let's put all students into an array
-
 def input_students
   puts 'Please enter the names of the students'
   puts "to finish, just hit return twice"
-  # creat an empty array
   students = []
   name = gets.chomp
-  #while the name is not empty repeat this code
   while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"

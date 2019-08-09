b3fa193db5900beeef9bf88fa1f2f692bfced574
Oliver Antcliff <antcliffoc@gmail.com>
2019-08-09 10:12:12 +0100

    add interective menu

--- directory.rb
+++ directory.rb
@@ -1,14 +1,20 @@
+@students = []
+
 def input_students
-  puts 'Please enter the names of the students'
+  puts 'Please enter the names of the students to add'
   puts "to finish, just hit return twice"
-  students = []
   name = gets.chomp
+  #while the name is not empty repeat this code
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
     name = gets.chomp
   end
-  students
+  @students
 end
 
 def print_header
@@ -16,15 +22,51 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+def print_students_list
+  @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} evil students."
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} evil student"
+  else
+    puts "Overall, we have #{@students.count} evil students."
+  end
+end
+
+def show_students
+  print_header()
+  print_students_list()
+  print_footer()
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students()
+    when "2"
+      show_students()
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    # 1. print the menus and ask user response
+    print_menu()
+    # 3. do what the user has asked
+    process(gets.chomp)
+  end
 end
 #call the methods
-students = input_students()
-print_header()
-print(students)
-print_footer(students)
+
+interactive_menu()
